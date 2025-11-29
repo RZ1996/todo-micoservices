8a43c8de353a36f51d891d0b8719b5be3413d5be
Róbert Zámočník <zamocnikrobert@gmail.com>
2025-11-29 18:58:37 +0100

    Add prod environment for Angular

---
+++ web/angular-app/todo/src/app/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  userServiceUrl: 'http://k8s-todo-userserv-16a6632009-624deda346c14de5.elb.eu-central-1.amazonaws.com',
+  taskServiceUrl: 'http://k8s-todo-taskserv-2f82725a6b-5da16f0a747c5b27.elb.eu-central-1.amazonaws.com',
+ 
+};
