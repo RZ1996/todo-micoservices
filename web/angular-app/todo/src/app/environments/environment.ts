b1fce840b2335a9fe851c7fe906ec6736fb686f6
Róbert Zámočník <zamocnikrobert@gmail.com>
2025-11-29 18:47:29 +0100

    Use AWS LB URLs for user and task service

--- web/angular-app/todo/src/app/environments/environment.ts
+++ web/angular-app/todo/src/app/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  production: false,
-  userUrl: 'http://localhost:9092',
-  taskUrl: 'http://localhost:9091',
+  production: true,
+
+
+  userServiceUrl: 'http://k8s-todo-userserv-16a6632009-624deda346c14de5.elb.eu-central-1.amazonaws.com',
+  taskServiceUrl: 'http://k8s-todo-taskserv-2f82725a6b-5da16f0a747c5b27.elb.eu-central-1.amazonaws.com',
 };
